61aa379d0e464c135ac9f8abcce32e72f467a933
Pilartri <167590609+Pilartri@users.noreply.github.com>
2024-09-14 13:40:18 -0300

    actualizaciones_2

--- vis-template/app/page.tsx
+++ vis-template/app/page.tsx
@@ -31,7 +31,7 @@ const HomePage = () => {
         </div>
 
       {/* Hero Section */}
-      <section className="relative max-w-screen-2xl h-[400px]">
+      <section>
         <Image
           src="/mujeres.png" 
           alt="Olympic Women Participation"
@@ -39,7 +39,7 @@ const HomePage = () => {
           height={400}
         />
         <div className="absolute inset-0">
-          <div className="container mx-auto py-96 ml-96 my-16 p-96">
+          <div className="container mx-auto py-96 ml-96 my-40 p-96">
             <h1 className="text-5xl font-black text-orange-500">PARTICI[PAR]</h1>
             <p className="mt-4 text-lg text-gray-700">
               Evolución en la participación femenina <br /> en los Juegos Olímpicos.
@@ -73,12 +73,13 @@ const HomePage = () => {
         </div>
         </div>
         <p className="text-lg mb-4 font-thin text-gray-600 text-justify px-96">
-        Un viaje a través de los años, una evolución que recién comienza. 30 ediciones de los Juegos Olímpicos en busca de la igualdad de oportunidades y participación. Miles de mujeres a lo largo del mundo demostrando su talento y su capacidad. <br />
+        Un viaje a través de los años, una evolución que recién comienza. 30 ediciones de los Juegos Olímpicos en busca de la igualdad de oportunidades y participación. Miles de mujeres a lo largo del mundo demostrando su talento y su capacidad.
         30 años más tarde podemos hablar de partici[par] por igual.
         </p>
-        <p className="text-lg font-bold text-gray-800 text-justify px-96 hover:text-gray-500">
-        Conoce más
-        </p>
+        <button className="bg-orange-500 hover:bg-orange-300 text-white font-bold py-2 px-4 rounded mt-8 ml-[385px]"
+        onClick={() => window.open()}>
+       Conocé más 
+        </button>
       </section>
 
       {/* medallero */}
@@ -90,21 +91,22 @@ const HomePage = () => {
         <div className="flex justify-center mb-8">
           
         <div className="max-w-4xl w-full ">
-                  < FlourishEmbed2 />
+        < FlourishEmbed2 />
 
         </div>
         </div>
-        <p className="text-lg mb-4 font-thin text-gray-600 text-justify px-96">
-        Miles de mujeres a lo largo del mundo mostrando una vez más lo que son capaces de hacer. Un medallero que conmociona. <br />
+        <p className="text-lg font-thin text-gray-600 text-justify px-96">
+        Miles de mujeres a lo largo del mundo mostrando una vez más lo que son capaces de hacer. Un medallero que conmociona.
         Grandes atletas batiendo récords y posicionándose en lo más alto del ranking mundial.
         </p>
-     
-        <p className="text-lg mb-6 font-bold text-gray-800 text-justify px-96 hover:text-gray-500">
-        Conoce más
-        </p>
+       
+        <button className="bg-orange-500 hover:bg-orange-300 text-white font-bold py-2 px-4 rounded mt-8 ml-[385px]"
+        onClick={() => window.open()}>
+       Conocé más 
+        </button>
       
       </section>
-      <Image className='mt-8 ml-64'
+      <Image className='mt-5 ml-64'
           src="/swim.gif" 
           alt="Olympic Women Participation"
           width={1000}
@@ -112,24 +114,30 @@ const HomePage = () => {
         />
 {/* conclusión */}
 <section className="container mx-auto">
-        <h2 className="text-3xl font-black text-center mb-6 text-orange-500">Medallero olímpico 2024</h2>
+        <h2 className="text-5xl font-black text-center mb-6 text-orange-500">PARTICI[PAR]</h2>
         <p className="text-center mb-8 text-gray-600 text-lg font-thin">
-          ¿Sabías que recién en 2024 se alcanzó la equidad de <br /> género en la competencia?
+        ¿Cómo podemos seguir impulsando la participación femenina?
         </p>
       
         <p className="text-lg mb-4 font-thin text-gray-600 text-justify px-96">
-        Miles de mujeres a lo largo del mundo mostrando una vez más lo que son capaces de hacer. Un medallero que conmociona. <br />
-        Grandes atletas batiendo récords y posicionándose en lo más alto del ranking mundial.
+        La historia del deporte está cambiando, y las mujeres están en el centro de esa transformación. <br /> La equidad en los Juegos Olímpicos no es solo un triunfo de cifras, sino una victoria simbólica en la lucha contra siglos de exclusión. Cada atleta femenina que sube al podio lleva consigo no solo su esfuerzo personal, sino el peso de <b> generaciones que soñaron con ese momento. </b> <br /> Pero este es solo el comienzo. La igualdad de oportunidades en los deportes debe ir más allá del número de participantes, se trata de garantizar que todas las niñas, en cualquier rincón del mundo, vean en el deporte un espacio que también les pertenece. <b> ¿Estamos listos para seguir rompiendo barreras? </b>
         </p>
      
-        <p className="text-lg mb-20 font-bold text-gray-800 text-justify px-96 hover:text-gray-500">
-        Conoce más
-        </p>
+        <button className="bg-orange-500 hover:bg-orange-300 text-white font-bold py-2 px-4 rounded mt-8 ml-[500px]"
+        onClick={() => window.open()}>
+       Conocé más sobre esta historia
+        </button>
       
       </section>
 
-      <footer className="bg-gray-800 py-8">
-        <div className="container mx-auto text-center text-white">
+      <footer>
+        <div>
+        <Image className='relative mx-auto mt-16'
+          src="/footer.png" 
+          alt="Olympic Women Participation"
+          height={400}
+          width={1600}
+        />
         </div>
       </footer>
     </div>
